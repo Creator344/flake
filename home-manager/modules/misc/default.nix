bd264aa3f6a9ba2c67f539beeea826adf7a40d83
Creator34 <98237272+Creator344@users.noreply.github.com>
2025-08-22 07:28:57 +1200

    trying to add firefox to tofi

---
+++ home-manager/modules/misc/default.nix
@@ -0,0 +1,6 @@
+{
+  imports = [
+    ./xdg-desktop-entries.nix
+  ];
+}
+
---
+++ home-manager/modules/misc/xdg-desktop-entries.nix
@@ -0,0 +1,10 @@
+{
+  firefox = {
+    name = "Firefox";
+    genericName = "Web Browser";
+    exec = "firefox %U";
+    terminal = false;
+    categories = [ "Application" "Network" "WebBrowser" ];
+    mimeType = [ "text/html" "text/xml" ];
+  };
+}
