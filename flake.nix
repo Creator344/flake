df2d6219502b599a3e8ae03814276af502721491
Creator34 <98237272+Creator344@users.noreply.github.com>
2025-08-21 16:25:44 +1200

    init commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Creator34's NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      duckbook = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ 
+	  ./configuration.nix
+
+          home-manager.nixosModules.home-manager
+	  {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+
+	    home-manager.users.noahj = import ./home.nix;
+	  }
+        ];
+      };
+    };
+  };
+}
