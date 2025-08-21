6d8a03c933aeffeccdf1c58d51fb018a4e2f62fb
Creator34 <98237272+Creator344@users.noreply.github.com>
2025-08-21 17:30:28 +1200

    formatted files slightly and added different configs for pc and laptop

--- flake.nix
+++ flake.nix
@@ -14,14 +14,14 @@
       duckbook = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ 
-	  ./configuration.nix
+	  ./devices/duckbook/configuration.nix
 
           home-manager.nixosModules.home-manager
 	  {
 	    home-manager.useGlobalPkgs = true;
 	    home-manager.useUserPackages = true;
 
-	    home-manager.users.noahj = import ./home.nix;
+	    home-manager.users.noahj = import ./home-manager/home.nix;
 	  }
         ];
       };
