54605ffeec8516df8696846ed1c5fee404859d3a
Creator34 <98237272+Creator344@users.noreply.github.com>
2025-08-22 20:52:37 +1200

    added locking shortcut to hyprland

--- home-manager/modules/hyprland/binds.nix
+++ home-manager/modules/hyprland/binds.nix
@@ -3,6 +3,7 @@
     bind = [
       "$mainMod, Space, exec, $menu"
       "$mainMod, C, killactive,"
+      "$mainMod, L, exec, hyprlock"
       "$mainMod, M, exec, loginctl terminate-user ''"
       "CTRL+ALT, T, exec, alacritty"
     ];
