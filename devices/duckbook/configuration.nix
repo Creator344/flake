5539ae7d82da180e2c42f418dbaaf0c81dec79b9
Creator34 <98237272+Creator344@users.noreply.github.com>
2025-08-23 18:43:16 +1200

    got steam working

--- devices/duckbook/configuration.nix
+++ devices/duckbook/configuration.nix
@@ -104,5 +104,25 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
+  
+  services.xserver.videoDrivers = [ "modesetting" "intel" ];
+
+  hardware.opengl = {
+    enable = true;
+    driSupport32Bit = true;
+
+    extraPackages = with pkgs; [
+      mesa
+      intel-media-driver
+      vulkan-loader
+      vulkan-validation-layers
+      vulkan-tools
+    ];
+
+    extraPackages32 = with pkgs.pkgsi686Linux; [
+      mesa
+      vulkan-loader
+    ];
+  };
 
 }
