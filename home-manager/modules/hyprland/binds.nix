d00db30595ad05fc48789330719df34e621bb789
Creator34 <98237272+Creator344@users.noreply.github.com>
2025-08-24 15:14:57 +1200

    added workspace switching binds: SUPER + <workspace>

--- home-manager/modules/hyprland/binds.nix
+++ home-manager/modules/hyprland/binds.nix
@@ -11,6 +11,17 @@
       "$mainMod, UP, swapwindow, u"
       "$mainMod, DOWN, swapwindow, d"
       "$mainMod+SHIFT, S, exec, grim -g '$(slurp)' - | swappy -f -"
+
+      "$mainMod, 1, exec, hyprctl dispatch workspace 1"
+      "$mainMod, 2, exec, hyprctl dispatch workspace 2"
+      "$mainMod, 3, exec, hyprctl dispatch workspace 3"
+      "$mainMod, 4, exec, hyprctl dispatch workspace 4"
+      "$mainMod, 5, exec, hyprctl dispatch workspace 5"
+      "$mainMod, 6, exec, hyprctl dispatch workspace 6"
+      "$mainMod, 7, exec, hyprctl dispatch workspace 7"
+      "$mainMod, 8, exec, hyprctl dispatch workspace 8"
+      "$mainMod, 9, exec, hyprctl dispatch workspace 9"
+      "$mainMod, 0, exec, hyprctl dispatch workspace 10"
     ];
   };
 }
