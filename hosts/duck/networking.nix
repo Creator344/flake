944f8fc59ad01ff58bbcb429ff515bc1337cd3c2
Creator34 <98237272+Creator344@users.noreply.github.com>
2025-12-02 09:48:55 +1300

    updated packages and removed unwanted vpn

--- hosts/duck/networking.nix
+++ hosts/duck/networking.nix
@@ -22,28 +22,7 @@
       "1.0.0.1"
     ];
 
-    wg-quick.interfaces = {
-      woldemure = {
-        privateKeyFile = "/home/noahj/wireguard-keys/privatekey";
-        address = [
-          "10.66.67.2/32"
-          "fd42:42:43::2/128"
-        ];
-        mtu = 1380;
-        listenPort = 38274;
-        peers = [
-          {
-            publicKey = "yphSocz6NN8xSJCOdhFeikiiw2Dm/NreQwZnUAB9Dno=";
-            endpoint = "73.44.20.131:51820";
-            allowedIPs = [
-              "10.66.67.2/32"
-              "fd42:42:43::2/128,10.0.0.190/32"
-            ];
-            persistentKeepalive = 15;
-          }
-        ];
-      };
-    };
+
     firewall.allowPing = true;
   };
 
